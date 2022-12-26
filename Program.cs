50477893a5a6b13acc2e63d7fd0ee52432493cd7
DinaAbdallah26 <121456447+DinaAbdallah26@users.noreply.github.com>
2022-12-27 01:11:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, k, sum;
+            Console.WriteLine("Enter n1");
+            n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2");
+            n2 = Int32.Parse(Console.ReadLine());
+            for (int i = n1; i <= n2; i++)
+            {
+
+                sum = 0;
+                for (k = 1; k < i; k++)
+                {
+                    if (i % k == 0)
+
+                        sum = sum + k;
+
+                }
+                if (sum == i)
+                    Console.WriteLine(i);
+
+            }
+        }
+    }
+    
+}
